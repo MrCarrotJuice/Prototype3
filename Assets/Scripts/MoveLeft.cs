d4a3b1714e5d76cb79514bb64f9fd5471e25b3dd
Flobatir Salama <328826@student.mboutrecht.nl>
2020-02-26 17:58:10 +0100

    initial Commit

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Beweegt de achtergrond en obstakels naar links als het nog geen gameover is.
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        // vernietigt gameobjects als ze aan de linkerkant het scherm verlaten.
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Herhaalt de achtergrond dmv boxcolliders.
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // axchtergrond steeds achter de vorige zetten dat het lijkt alsof de speler rent.
+        if (transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+}
